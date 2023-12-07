b946c8552e778ded91394fae31ebb444cd4dfc86
enunez <enunez.ing@gmail.com>
2023-12-06 21:45:45 -0300

    models.py: Added validate data() function

--- models/models.py
+++ models/models.py
@@ -15,8 +15,11 @@ Float or NullType). You should select one as a primary key.
 5) Finally, add the models to the list MODELS at the end of this module.
 """
 
-from .base import TableModel
+from .base import Table, TableModel
 from .typing import SQLDataType, Integer, Text, Float, NullType
+from typing import Any
+
+# Create your models here
 
 
 class User(TableModel):
@@ -41,3 +44,39 @@ class Password(TableModel):
 
 # Add the created models
 MODELS = [User, Password]
+
+
+def validate_data(table: Table, data: dict[str, Any]) -> bool:
+    """Validates 'data' against 'table' so that it respects the schema defined by the model.
+
+    Args:
+        table (Table) : A table model as defined in this module.
+        data (data[str, Any]) : data to be validated against the model.
+
+    Return:
+        True if 'data' is successfully validated and False if not.
+    """
+    table_schema = table.__schema__
+    # Check if keys exist in the table
+    for key in data:
+        if key not in table_schema:
+            return False
+
+    # Check type of values
+    for key, value in data.items():
+        sql_data_type = table_schema.get(key, NullType("sqlite3"))
+        if not isinstance(value, sql_data_type.py_type):
+            return False
+
+    for key, value in table_schema.items():
+        # Check primary key (it shouldn't be part of data)
+        if value.primary_key and key in data:
+            return False
+        elif value.primary_key and key not in data:
+            continue
+        # Check nullability
+        if not value.nullable and key not in data:
+            return False
+        # TODO: Check uniqueness (HOW? Probably we should leave that to the DBConnection)
+
+    return True
